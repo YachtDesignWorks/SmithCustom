048bf097ebd5dc67ac7d7bbe7ecbb9b47f6a2319
Yacht Design Works <tak@yachtdesignworks.com>
2021-05-19 08:19:35 -0400

    updates

--- smith-custom-53---kingston-grey/app-files/data.js
+++ smith-custom-53---kingston-grey/app-files/data.js
@@ -24,34 +24,34 @@ var APP_DATA = {
       ],
       "faceSize": 1750,
       "initialViewParameters": {
-        "yaw": -2.837037528295671,
-        "pitch": 0.17132185371905884,
+        "yaw": -2.770046883015569,
+        "pitch": 0.23596886365555036,
         "fov": 1.4236357722889479
       },
       "linkHotspots": [
         {
-          "yaw": -2.4643812580284923,
-          "pitch": 0.01998280837533173,
+          "yaw": -2.460040306425464,
+          "pitch": 0.03687196348053767,
           "rotation": 3.141592653589793,
           "target": "2-bow"
         },
         {
-          "yaw": 2.830952519470876,
-          "pitch": 0.04170580410837488,
+          "yaw": -2.994650802006195,
+          "pitch": -0.07612166672179299,
           "rotation": 3.141592653589793,
-          "target": "3-transom"
+          "target": "3-cockpit"
         },
         {
-          "yaw": 2.2193904784775,
-          "pitch": 0.17188455481426246,
+          "yaw": 2.8213557450491304,
+          "pitch": 0.027919245798530667,
+          "rotation": 3.141592653589793,
+          "target": "4-transom"
+        },
+        {
+          "yaw": 2.233056737807347,
+          "pitch": 0.16474213793285486,
           "rotation": 7.853981633974483,
           "target": "1-rear-perspective"
-        },
-        {
-          "yaw": -2.995293422119385,
-          "pitch": -0.08183499121863491,
-          "rotation": 3.141592653589793,
-          "target": "4-cockpit"
         }
       ],
       "infoHotspots": []
@@ -80,34 +80,34 @@ var APP_DATA = {
       ],
       "faceSize": 1750,
       "initialViewParameters": {
-        "yaw": 2.8482738293893917,
-        "pitch": 0.2797275849440535,
+        "yaw": -2.866688351599791,
+        "pitch": 0.3028850970733181,
         "fov": 1.4236357722889479
       },
       "linkHotspots": [
         {
-          "yaw": 2.582072391159559,
-          "pitch": 0.24237959525154906,
+          "yaw": 2.9414057572404673,
+          "pitch": 0.15576809762092658,
           "rotation": 3.141592653589793,
-          "target": "3-transom"
+          "target": "4-transom"
         },
         {
-          "yaw": -2.924992949759039,
-          "pitch": 0.06725757093920137,
+          "yaw": -2.8982255298996797,
+          "pitch": -0.024616018373365023,
+          "rotation": 3.141592653589793,
+          "target": "3-cockpit"
+        },
+        {
+          "yaw": -2.5977597787432885,
+          "pitch": 0.013051141152747192,
           "rotation": 3.141592653589793,
           "target": "2-bow"
         },
         {
-          "yaw": -2.365672553831457,
-          "pitch": 0.12787430397820998,
+          "yaw": -2.140491291778318,
+          "pitch": 0.11036200408579688,
           "rotation": 4.71238898038469,
           "target": "0-front-perspective"
-        },
-        {
-          "yaw": 3.007534113370757,
-          "pitch": 0.058353875208606354,
-          "rotation": 3.141592653589793,
-          "target": "4-cockpit"
         }
       ],
       "infoHotspots": []
@@ -136,90 +136,40 @@ var APP_DATA = {
       ],
       "faceSize": 1750,
       "initialViewParameters": {
-        "yaw": -3.029300204392511,
-        "pitch": 0.5128202447807446,
+        "yaw": -3.0629918117517434,
+        "pitch": 0.41324913432185184,
         "fov": 1.4236357722889479
       },
       "linkHotspots": [
         {
-          "yaw": 2.5801525005903496,
-          "pitch": -0.1602877688060449,
+          "yaw": -2.6954832060617733,
+          "pitch": 0.11628708534428256,
+          "rotation": 18.06415775814132,
+          "target": "4-transom"
+        },
+        {
+          "yaw": -3.1336124618111683,
+          "pitch": -0.19490057116490078,
+          "rotation": 3.141592653589793,
+          "target": "3-cockpit"
+        },
+        {
+          "yaw": 2.64273021375206,
+          "pitch": 0.09090505979880703,
           "rotation": 8.63937979737193,
           "target": "1-rear-perspective"
         },
         {
-          "yaw": -3.0715697061429506,
-          "pitch": -0.22078244056276475,
-          "rotation": 3.141592653589793,
-          "target": "3-transom"
-        },
-        {
-          "yaw": 1.1600278959233936,
-          "pitch": -0.28521742017619545,
-          "rotation": 8.63937979737193,
+          "yaw": 1.113258042560421,
+          "pitch": -0.19548359390943482,
+          "rotation": 2.356194490192345,
           "target": "0-front-perspective"
-        },
-        {
-          "yaw": -3.0648825461374187,
-          "pitch": 0.212332436305676,
-          "rotation": 0,
-          "target": "4-cockpit"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-transom",
-      "name": "Transom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1750,
-      "initialViewParameters": {
-        "yaw": -0.08385280876759893,
-        "pitch": 0.49005485271915106,
-        "fov": 1.3487598040662003
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.1800580143017676,
-          "pitch": -0.39138767182444134,
-          "rotation": 3.141592653589793,
-          "target": "1-rear-perspective"
-        },
-        {
-          "yaw": 2.9679999878583363,
-          "pitch": 0.1701193759577304,
-          "rotation": 0,
-          "target": "4-cockpit"
-        },
-        {
-          "yaw": -1.7515519626789882,
-          "pitch": 0.18793011291310258,
-          "rotation": 4.71238898038469,
-          "target": "2-bow"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-cockpit",
+      "id": "3-cockpit",
       "name": "Cockpit",
       "levels": [
         {
@@ -242,22 +192,78 @@ var APP_DATA = {
       ],
       "faceSize": 1750,
       "initialViewParameters": {
-        "yaw": 0.08095223505836735,
-        "pitch": 0.4338183538867515,
-        "fov": 1.3487598040662003
+        "yaw": 0.06577596873764513,
+        "pitch": 0.43058218528514836,
+        "fov": 1.4236357722889479
       },
       "linkHotspots": [
         {
-          "yaw": -3.121349850402158,
-          "pitch": 0.18078963487867483,
-          "rotation": 0,
-          "target": "3-transom"
-        },
-        {
-          "yaw": 0.06825774798755546,
-          "pitch": -0.019951187173649743,
+          "yaw": 0.09332863427071558,
+          "pitch": -0.057130017177982495,
           "rotation": 0,
           "target": "2-bow"
+        },
+        {
+          "yaw": 3.1303663857316906,
+          "pitch": 0.15738571863156992,
+          "rotation": 0,
+          "target": "4-transom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-transom",
+      "name": "Transom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1750,
+      "initialViewParameters": {
+        "yaw": -0.0293775381927599,
+        "pitch": 0.6200166980002031,
+        "fov": 1.4236357722889479
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1102017855489965,
+          "pitch": 0.20995842562984102,
+          "rotation": 0,
+          "target": "3-cockpit"
+        },
+        {
+          "yaw": -2.3879767399009726,
+          "pitch": 0.20870893882205976,
+          "rotation": 5.497787143782138,
+          "target": "2-bow"
+        },
+        {
+          "yaw": -1.2869677467039669,
+          "pitch": 0.09312154139921702,
+          "rotation": 3.9269908169872414,
+          "target": "1-rear-perspective"
+        },
+        {
+          "yaw": -2.1414342483609623,
+          "pitch": -0.25436539789796875,
+          "rotation": 3.9269908169872414,
+          "target": "0-front-perspective"
         }
       ],
       "infoHotspots": []
